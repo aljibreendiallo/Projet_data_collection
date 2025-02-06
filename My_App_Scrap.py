dbb039fb7c814803a08c28a53820045cddc8cde0
aljibreendiallo <alphaoumar.diallo5@unchk.edu.sn>
2025-02-06 12:24:56 +0000

    Update My_App_Scrap.py

--- My_App_Scrap.py
+++ My_App_Scrap.py
@@ -12,7 +12,12 @@ import seaborn as sns
 options = webdriver.ChromeOptions()
 options.add_argument("--headless=new")
 options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-dev-shm-usage") 
+chrome_options.add_argument('--disable-gpu')  # Désactive l'accélération GPU
+chrome_options.add_argument('--disable-dev-shm-usage')  # Évite les erreurs de mémoire
+chrome_options.add_argument('--disable-extensions')  # Désactive les extensions
+chrome_options.add_argument('--disable-infobars')  # Désactive les infobars
+chrome_options.add_argument('--remote-debugging-port=9222')
 
 # Initialisation du driver avec webdriver_manager
 service = Service(ChromeDriverManager().install())
